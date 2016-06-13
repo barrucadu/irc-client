6a720e22112761c352b77d8cb48ff82b1fdf384d
Michael Walker <mike@barrucadu.co.uk>
2016-06-13 19:09:40 +0100

    Bump to version 0.4.1.0

--- Network/IRC/Client.hs
+++ Network/IRC/Client.hs
@@ -136,7 +136,7 @@ defaultIRCConf n = InstanceConfig
   , _realname      = n
   , _password      = Nothing
   , _channels      = []
-  , _ctcpVer       = "irc-client-0.3.0.0"
+  , _ctcpVer       = "irc-client-0.4.1.0"
   , _eventHandlers = defaultEventHandlers
   , _ignore        = []
   }
