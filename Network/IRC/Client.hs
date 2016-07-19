3af5b8dee9271770a8497af2f49ae5ffbec90cc9
Michael Walker <mike@barrucadu.co.uk>
2016-07-19 12:51:54 +0100

    Drop patch level from CTCP version response

--- Network/IRC/Client.hs
+++ Network/IRC/Client.hs
@@ -136,7 +136,7 @@ defaultIRCConf n = InstanceConfig
   , _realname      = n
   , _password      = Nothing
   , _channels      = []
-  , _ctcpVer       = "irc-client-0.4.2.1"
+  , _ctcpVer       = "irc-client-0.4.2"
   , _eventHandlers = defaultEventHandlers
   , _ignore        = []
   }
