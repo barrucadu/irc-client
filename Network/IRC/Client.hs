3870142bb643689569ad65d7333df20dea8e9b6d
Tristan Cacqueray <tdecacqu@redhat.com>
2020-09-21 22:50:36 +0000

    docs: add typical outside usage form

--- Network/IRC/Client.hs
+++ Network/IRC/Client.hs
@@ -280,6 +280,22 @@ runClient cconf iconf ustate = newIRCState cconf iconf ustate >>= runClientWith
 -- Multiple clients should not be run with the same 'IRCState'. The
 -- utility of this is to be able to run @IRC s a@ actions in order to
 -- interact with the client from the outside.
+--
+-- Typical usage will be of this form:
+--
+-- > ircThread :: IRCState s -> IO ()
+-- > ircThread state = Control.Exception.handle doReconnect (runClientWith state)
+-- >   where
+-- >     doReconnect :: Disconnect -> IO ()
+-- >     doReconnect e = do
+-- >       print ("irc client exited: " <> show e)
+-- >       threadDelay 1000000 >> runIRCAction reconnect state >> ircThread state
+--
+-- > mainThread :: ConnectionConfig s -> InstanceConfig s -> IO ()
+-- > mainThread conn cfg = do
+-- >   state <- newIRCState conn cfg ()
+-- >   withAsync (ircThread state) (const (runMain state))
+--
 runClientWith :: MonadIO m => IRCState s -> m ()
 runClientWith = runIRCAction runner
 
